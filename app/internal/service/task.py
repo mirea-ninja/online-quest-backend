fc07e5f81fcbe2dea86006c0f4e904549c86f926
Oniel <51058739+0niel@users.noreply.github.com>
2022-10-06 12:17:53 +0300

    Update tasks service

--- app/internal/service/task.py
+++ app/internal/service/task.py
@@ -12,12 +12,24 @@ class TaskService:
     users_service: UserService
 
     def __init__(
-        self,
-        repository: AnswerRepository,
-        users_service: UserService,
+            self,
+            repository: AnswerRepository,
+            users_service: UserService,
     ) -> None:
         self.repository = repository
         self.users_service = users_service
+
+    async def is_all_tasks_done(self, user_id: int) -> bool:
+        all_user_answers = await self.repository.get_user_answers(user_id)
+
+        if all_user_answers:
+            last_answer = max(
+                all_user_answers,
+                key=lambda answer: answer.task_unique_number,
+            )
+            return last_answer.task_unique_number == 10 and last_answer.is_correct
+        else:
+            return False
 
     async def get(self, cmd: TaskInRequest) -> Union[TaskModel, UserIsBad]:
         user = await self.users_service.get(cmd=GetUserCommand(id=cmd.user_id))
