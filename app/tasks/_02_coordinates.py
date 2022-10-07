5a3cbc37b21840a07c2e0a3e6158849bb8aa16f5
Oniel <51058739+0niel@users.noreply.github.com>
2022-10-08 00:28:02 +0300

    Update _02_coordinates.py

--- app/tasks/_02_coordinates.py
+++ app/tasks/_02_coordinates.py
@@ -1,10 +1,11 @@
 from app.internal.schemes import TaskModel
 
-FLAG = "ninja{48.785,132.932}"
+
+FLAGS = ["ninja{48.785,132.932}", "ninja{48.785183,132.932175}", "ninja{48.78518,132.93217}", "ninja{48.7851,132.9321}", "ninja{48.78,132.93}"] 
 
 
 def get_flag() -> str:
-    return FLAG
+    return FLAGS
 
 
 def get_task() -> TaskModel:
@@ -17,4 +18,20 @@ def get_task() -> TaskModel:
 
 
 def check_solution(user_answer: str) -> bool:
-    return user_answer == get_flag()
+    user_answer = user_answer.lower().replace(' ', '')
+    
+
+    try:
+        if '{' in user_answer and '}' in user_answer:
+            tmp_ans = user_answer.split('{')[1].split('}')[0]
+            by_nums = tmp_ans.split(',')
+            first_num = float(by_nums[0])
+            second_num = float(by_nums[1])
+
+            if abs(first_num - 48.785183) <= 0.001 and abs(second_num - 132.932175) <= 0.001:
+                return True
+                
+    except Exception as ex:
+        pass
+
+    return user_answer in get_flag()
